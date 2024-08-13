e20e06ae3dab87de518bce9782e048d3073b986c
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-13 14:57:45 -0300

    modificando as props dos sub elementos

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -12,24 +12,28 @@ class CardNews extends HTMLElement {
         componentRoot.setAttribute("class", "card")
 
         const cardLeft = document.createElement("div");
-        cardLeft.setAttribute("class", "card_left")
+        cardLeft.setAttribute("class", "card_left");
 
         const autor = document.createElement("span");
         autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
 
         const linkTitle = document.createElement("a");   
-        linkTitle.textContent =  this.getAttribute("title")
-        
+        linkTitle.textContent =  this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
         const newsContent = document.createElement("p");
-        newsContent.textContent =  this.getAttribute("content")
+        newsContent.textContent =  this.getAttribute("content");
+
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
         cardLeft.appendChild(newsContent);
 
         const cardRight = document.createElement("div");
-        cardRight.setAttribute("class", "card_right")
+        cardRight.setAttribute("class", "card_right");
         
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo");
+        newsImage.alt = "foto da noticia"
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
