b7bb15560aa510bb66bbccc5b53c11aefbf914a7
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-13 11:53:43 -0300

    Estilizando uma component

---
+++ src/components/TituloDinamico.js
@@ -0,0 +1,21 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+        //base da component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Lucas"
+
+        const style = document.createElement("style")
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    } 
+}
+
+customElements.define('titulo-dinamico', TituloDinamico);
